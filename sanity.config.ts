93e10592272c716f2af15e60db9d3debae4ee5bc
Justin Saju <justinsaju21@gmail.com>
2026-02-06 18:47:43 +0530

    fix(build): use sanity/vision instead of @sanity/vision

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
+import { visionTool } from 'sanity/vision'
 import { schemaTypes } from './sanity-studio/schemaTypes'
 
 export default defineConfig({
